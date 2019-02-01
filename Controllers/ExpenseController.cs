2de082f2ebbf75c8617ef00c665a118b68496fd2
rajesh062 <rajeshmaurya9919@gmail.com>
2019-02-02 03:15:00 +0530

    New Controller Class Add

---
+++ Controllers/ExpenseController.cs
@@ -0,0 +1,15 @@
+
+using Microsoft.AspNetCore.Mvc;
+
+public class ExpenseController:Controller
+{
+    public ExpenseController()
+    {
+        //ExpenseName="dsfsf";
+    
+        
+    }
+ public string ExpenseName { get; set; }
+ 
+ 
+}
